4e916ab01ab71929c05462d327cbcb9f8e7ccdcb
Todd Hagen <todd.a.hagen@gmail.com>
2015-11-06 12:10:08 -0700

    save to new file for each subj

--- getalleyedata.m
+++ getalleyedata.m
@@ -1,33 +1,33 @@
 function eye_data = getalleyedata(datadir)
-    %Runs importeyedata on all *_eye.tsv in specified folder.
+    %Saves eye data as .mat for all *_eye.tsv files in indicated dir
     currdir = pwd;
-    %Include '/' at the end of the path!
+    %add '/' at the end of the path if missing
     if datadir(length(datadir)) ~= '/'
         datadir = [datadir '/'];
     end
-    % pull all data filenames and create empty output struct
+    % pull all data filenames
     filenames = dir([datadir '*_eye.tsv']);
     fnames = {filenames.name}';
-    %output = cellfun(@importeyedata, fnames);
-    output = struct('subject', cell(1, length(fnames)), 'data', cell(1, length(fnames)), 'message', cell(1, length(fnames)));
-    cd(datadir)
+    %choose the pattern for saving files
+    %chop off '.tsv' at the end of the data filename
+    savenames = cellfun(@(x) x(1:end-4), fnames, 'UniformOutput', false);
     %run importeyedata.m on each file and separate data and message
+    cd(datadir)
     for i = 1:length(fnames)
         try
             disp(['Importing ' fnames{i}])
-            raw = importeyedata(fnames{i});
-            output(i).subject = fnames{i};
-            output(i).data = raw.data;
-            output(i).message = raw.message;
+            raw = importeyedata([datadir fnames{i}]);
+            eye_data = raw.data;
+            message = raw.message;
+            %save as eye_data and message to subject's filename as .mat
+            disp(['Saving data as ' savenames{i} '.mat'])
+            save([savenames{i}], 'eye_data', 'message');
+            clear eye_data message
         catch 
             disp(['An error occurred while importing data from ' fnames{i}]);
-            output(i).subject = fnames{i};
             disp('Skipping this file.')
             continue
         end
     end
-    disp(['Saving data as eye_data.m in ' datadir])
-    eye_data = output;
-    save('eye_data', 'output');
     cd(currdir)
 end
