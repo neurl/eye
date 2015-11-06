cc0113bf94f4d465d3e21d23e0fc9958f6fb78ab
Todd Hagen <todd.a.hagen@gmail.com>
2015-11-06 13:30:14 -0700

    shortened code; still for 1 event

--- findEventTimes.m
+++ findEventTimes.m
@@ -1,6 +1,4 @@
-function event_times = findEventTimes(message, datastruct)
-%pulls times of all events matching input message in a subject's datastruct.
-message_cell = repmat({message}, length(datastruct.message.string), 1);
-comp = cellfun(@strcmp, message_cell, datastruct.message.string); %, 'UniformOutput', true
-times = str2double(datastruct.message.time);
-event_times = times(comp);
+function times = findEventTimes(event, datastruct)
+%pulls times of all events matching ONE input event string in a subject's 'message' struct.
+ind = strcmp(datastruct.string, event);
+times = datastruct.time(ind);
