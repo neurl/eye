26c7cbd78ca2067d760365bdfb66745964fd0c9f
Todd Hagen <todd.a.hagen@gmail.com>
2015-11-11 15:53:38 -0700

    Function on eye_data, rather than eye_data.message

--- findEventTimes.m
+++ findEventTimes.m
@@ -1,4 +1,6 @@
 function times = findEventTimes(event, datastruct)
-%pulls times of all events matching ONE input event string in a subject's 'message' struct.
-ind = strcmp(datastruct.string, event);
-times = datastruct.time(ind);
+%pulls times of all events matching ONE input event string in a subject's
+%loaded _eye.mat file
+ind = strcmp(datastruct.message.string, event);
+times = datastruct.message.time(ind);
+times = cellfun(@str2num, times);
